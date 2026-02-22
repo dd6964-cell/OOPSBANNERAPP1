e7c2df2504b5f64e069335daf57beadb0f329b16
dd6964-cell <dd6964@srmist.edu.in>
2026-02-22 22:09:03 +0530

    UC2

--- src/OopsBannerAPP.java
+++ src/OopsBannerAPP.java
@@ -1,5 +1,13 @@
 public class OopsBannerAPP{
     public static void main(String[] args){
-        System.out.println("OOPS");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"    *****");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" **      ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**       ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**    ** "+" "+" **      ");
+        System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"  ***    ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"      ** ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"       **");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"      ** ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+"*****    ");
     }
 }
